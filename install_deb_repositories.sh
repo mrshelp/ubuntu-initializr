aaaa528567e9ff05c6166e5b6b1881702021ab64
Krystian Pypłacz <mister.shelp@gmail.com>
2025-01-12 18:20:15 +0100

    deb-repo install script prototype updated

--- install_deb_repositories.sh
+++ install_deb_repositories.sh
@@ -7,6 +7,7 @@ source /etc/os-release
 source commons/utils.sh
 
 ESC_SPACE='%SPACE%'
+KEYRINGS_DIR=/etc/apt/keyrings
 SOURCES_DIR=/etc/apt/sources.list.d
 
 escape() { echo "$1" | sed "s/\ /${ESC_SPACE}/g"; }
@@ -16,32 +17,38 @@ chk_deb_repo() {
   local keyring=$1
   local sources=$2
   local repo=$3
-  local stat=$(stat -c %A "${keyring}" 2> /dev/null)
+  local stat=$(stat -c %A "${KEYRINGS_DIR}/${keyring}" 2> /dev/null)
   local repo_escaped=$(echo "${repo}" | sed "s/\[/\\\[/g;s/\]/\\\]/g")
   local contains=$(cat "${SOURCES_DIR}/${sources}" 2> /dev/null | grep -q "${repo_escaped}" && echo 1 || echo 0)
-  [[ -s "${keyring}" && "${stat:7:1}" == 'r' && -s "${SOURCES_DIR}/${sources}" && ${contains} = 1 ]]
+  [[ -s "${KEYRINGS_DIR}/${keyring}" && "${stat:7:1}" == 'r' && -s "${SOURCES_DIR}/${sources}" && ${contains} = 1 ]]
 }
 
 in_deb_repo() {
   local url=$1
   local keyring=$2
-  local sources=$3
-  local repo=$(unescape "$4")
-  sudo install -m 0755 -d $(dirname "${keyring}")
-  curl -fsSL ${url} | sudo gpg --dearmor -o "${keyring}"
-  sudo chmod a+r "${keyring}"
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=${keyring}] ${repo}" | sudo tee "${SOURCES_DIR}/${sources}" > /dev/null
+  local dearmor=$3
+  local sources=$4
+  local repo=$(unescape "$5")
+  sudo install -m 0755 -d "${KEYRINGS_DIR}"
+  if [ "${dearmor}" = 'true' ]; then
+    curl -fsSL "${url}" | sudo gpg --dearmor -o "${KEYRINGS_DIR}/${keyring}"
+  else
+    sudo curl -fsSL "${url}" -o "${KEYRINGS_DIR}/${keyring}"
+  fi
+  sudo chmod a+r "${KEYRINGS_DIR}/${keyring}"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEYRINGS_DIR}/${keyring}] ${repo}" | sudo tee "${SOURCES_DIR}/${sources}" > /dev/null
   ${CMD_REFRESH}
 }
 
 check_install_deb_repo() {
   local url=$1
   local keyring=$2
-  local sources=$3
-  local repo=$4
+  local dearmor=$3
+  local sources=$4
+  local repo=$5
   echo_b "Checking if ${sources} repo is installed..."
   if ! chk_deb_repo "${keyring}" "${sources}" "${repo}"; then
-    install "${sources} repo" $IM_ERR in_deb_repo "${url} ${keyring} ${sources} $(escape "${repo}")"
+    install "${sources} repo" $IM_ERR in_deb_repo "${url} ${keyring} ${dearmor} ${sources} $(escape "${repo}")"
   else
     echo_g "${sources} repo is already installed.\n"
   fi
@@ -51,12 +58,14 @@ check_install_deb_repos() {
   #TODO:/etc/apt/keyrings/ var
   check_install_deb_repo \
     'https://download.docker.com/linux/ubuntu/gpg' \
-    '/etc/apt/keyrings/docker.asc' \
+    'docker.asc' \
+    false \
     'docker.list' \
     "https://download.docker.com/linux/ubuntu ${UBUNTU_CODENAME} stable"
   check_install_deb_repo \
     'https://ppa.floorp.app/KEY.gpg' \
-    '/etc/apt/keyrings/Floorp.gpg' \
+    'Floorp.gpg' \
+    true \
     'Floorp.list' \
     "https://ppa.floorp.app/\$(ARCH) ./"
 }
@@ -65,7 +74,10 @@ DEV_remove() {
   for FILE in \
     /etc/apt/sources.list.d/Floorp.list \
     /etc/apt/keyrings/Floorp.gpg \
-    /usr/share/keyrings/Floorp.gpg
+    /usr/share/keyrings/Floorp.gpg \
+    /etc/apt/sources.list.d/docker.list \
+    /etc/apt/keyrings/docker.asc \
+    /usr/share/keyrings/docker.asc
   do
     test -f ${FILE} && sudo rm ${FILE}
   done
